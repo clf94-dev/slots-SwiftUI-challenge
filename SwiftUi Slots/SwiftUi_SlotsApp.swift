79fd414e03b329dcb5adf852789dbfbf52b60e51
clf94-dev <clf94@hotmail.es>
2023-06-12 19:49:00 +0200

    Initial Commit

---
+++ SwiftUi Slots/SwiftUi_SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_SlotsApp.swift
+//  SwiftUi Slots
+//
+//  Created by Carmen Lucas on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
