40f881dc3a8f3f85265a90646ff6c1c3c38e8926
clf94-dev <clf94@hotmail.es>
2023-06-12 20:16:48 +0200

    added styles day 11

--- SwiftUi Slots/ContentView.swift
+++ SwiftUi Slots/ContentView.swift
@@ -10,24 +10,43 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack{
-            Spacer()
             Text("SwiftUI Slots!")
+                .font(.system(size: 35))
+                .padding(.top, 35)
             Spacer()
             Text("Credits:")
             Spacer()
             HStack{
                 Spacer()
-                Image("cherry").resizable().frame(width: 100, height: 100)
+                Image("cherry")
+                    .resizable()
+                    .frame(width: 100, height: 100)
                 Spacer()
-                Image("apple").resizable().frame(width: 100, height: 100)
+                Image("apple")
+                    .resizable()
+                    .frame(width: 100, height: 100)
                 Spacer()
-                Image("star").resizable().frame(width: 100, height: 100)
+                Image("star")
+                    .resizable()
+                    .frame(width: 100, height: 100)
                 Spacer()
             }
                 .padding(.all)
                 
             Spacer()
-            Button(action: {}, label: { Text("spin") })
+            Button(action: {}, label: {
+                Text("Spin")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                
+            })
+                .padding(.vertical, 10)
+                .padding(.horizontal, 30)
+                .foregroundColor(Color.white)
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.pink/*@END_MENU_TOKEN@*/)
+                .border(/*@START_MENU_TOKEN@*/Color.pink/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                .cornerRadius(25)
+                
+                
             Spacer()
         }
         
